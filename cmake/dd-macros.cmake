fdfda3991f0bf017869a0e6bd5374073519670aa
Azmyin M. Kamal <azmyin12@gmail.com>
2024-09-03 11:09:38 -0500

    before successful build

--- cmake/dd-macros.cmake
+++ cmake/dd-macros.cmake
@@ -1,14 +1,15 @@
+# Python interpretor
 macro(setup_python)
-  find_package(PythonLibs 3.4 REQUIRED)
-  find_package(PythonInterp 3.4 REQUIRED)
+  find_package(PythonLibs 3.10 REQUIRED)
+  find_package(PythonInterp 3.10 REQUIRED)
 endmacro()
 
-
+# Point to QT 5 for Ubuntu 22.04
 macro(setup_qt)
   get_ubuntu_version()
 
-  set(_default_qt_version 4)
-  if (ubuntu_version EQUAL 18.04)
+  set(_default_qt_version 5)
+  if (ubuntu_version EQUAL 22.04)
     set(_default_qt_version 5)
   endif()
 
@@ -38,9 +39,9 @@ macro(setup_qt)
     message(FATAL_ERROR "DD_QT_VERSION is set to an unexpected value: ${DD_QT_VERSION}")
   endif()
 
-
 endmacro()
 
+# Dependencies based on what QT 5.x was used.
 macro(qt_wrap_ui)
   if(DD_QT_VERSION EQUAL 4)
     qt4_wrap_ui(${ARGN})
@@ -93,6 +94,25 @@ macro(use_cpp11)
   endif()
 endmacro()
 
+# ChatGPT 4.0
+macro(use_cpp14)
+  if (CMAKE_COMPILER_IS_GNUCC)
+    execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
+                    OUTPUT_VARIABLE GCC_VERSION)
+    if (NOT (GCC_VERSION VERSION_GREATER 4.8 OR GCC_VERSION VERSION_EQUAL 4.8))
+      message(FATAL_ERROR "requires gcc version >= 4.8")  # to support the c++14 flag below
+    elseif(GCC_VERSION VERSION_LESS 5.0)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y")  # For GCC < 5.0, use the experimental C++14 flag
+    else()
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+    endif()
+  elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+    if (APPLE)
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    endif()
+  endif()
+endmacro()
 
 macro(get_ubuntu_version)
   find_program(LSB_RELEASE lsb_release)
